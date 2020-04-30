d28399e60387700f725b9f89d0773dfc65069414
Archie Baer <archie@abaer.dev>
2020-04-30 01:45:40 +0100

    Database

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const {SERVERNAME} = process.env;
+const credentials = require("./credentials");
+const fs = require("fs");
+
+// Sequelize
+const {Sequelize, DataTypes} = require("sequelize");
+const db = new Sequelize(
+	credentials.db.name,
+	credentials.db.username,
+	credentials.db.password,
+	{
+		host: credentials.db.host,
+		dialect: "mariadb",
+		logging: false,
+		dialectOptions: {
+			timezone: "Etc/GMT0"
+		}
+	}
+);
+
+// File Model
+const File = db.define("episode", {
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    primaryServer: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    updatedAt: false
+});
 
 // Express
 const express = require("express");
 const app = express();
-app.listen(8080);
+db.sync().then(() => app.listen(8080));
 
+// Base
 app.get("/", (req, res) => {
     res.send(`AllesFS ${SERVERNAME}`);
 });
