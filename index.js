948d491e37d864da6e516bff0cf59950f8e54465
Archie Baer <archie@abaer.dev>
2020-04-30 10:16:05 +0100

    Client Authorization

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {SERVERNAME} = process.env;
 const credentials = require("./credentials");
+const idLength = 125;
 const fs = require("fs");
+const randomString = require("randomstring").generate;
 
 // Sequelize
 const {Sequelize, DataTypes} = require("sequelize");
@@ -18,10 +20,29 @@ const db = new Sequelize(
 	}
 );
 
-// File Model
-const File = db.define("episode", {
+// Client Model
+const Client = db.define("client", {
     id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+    },
+    secret: {
         type: DataTypes.STRING,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
+
+// File Model
+const File = db.define("file", {
+    id: {
+        type: DataTypes.STRING(128),
         allowNull: false,
         primaryKey: true
     },
@@ -32,18 +53,60 @@ const File = db.define("episode", {
 }, {
     updatedAt: false
 });
+File.hasOne(Client);
 
 // Express
 const express = require("express");
 const app = express();
-db.sync().then(() => app.listen(8080));
+db.sync().then(() =>
+    app.listen(8080, () =>
+        console.log("Server is online.")
+    )
+);
 
 // Base
 app.get("/", (req, res) => {
     res.send(`AllesFS ${SERVERNAME}`);
 });
 
+// Client Authorization
+app.use(async (req, res, next) => {
+    const header = req.headers.authorization;
+    if (typeof header !== "string") return next();
+    if (header.split(" ").length !== 2 || !header.startsWith("Basic ")) return next();
+
+    // Parse Header
+    let credentials;
+    try {
+        const credentialsString = Buffer.from(header.split(" ")[1], "base64").toString();
+        if (credentialsString.split(":").length !== 2) return next();
+        credentials = {
+            id: credentialsString.split(":")[0],
+            secret: credentialsString.split(":")[1]
+        };
+    } catch (err) {
+        return next();
+    }
+
+    // Get Client
+    const client = await Client.findOne({
+        where: {
+            id: credentials.id
+        }
+    });
+    if (!client || client.secret !== credentials.secret) return next();
+    
+    req.authClient = client;
+    next();
+});
+
 // Get File
 app.get("/:filename", (req, res) => {
     console.log(req.params.filename);
+});
+
+// Upload File
+app.post("/", (req, res) => {
+    if (!req.authClient) return res.status(401).send("Unauthorized");
+    res.json({});
 });
