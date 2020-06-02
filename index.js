a70195dd594d79184924b617415cd95bc4962448
Archie Baer <archie@abaer.dev>
2020-06-02 14:57:50 +0100

    Decodes from base64

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const app = express();
 app.listen(8080);
 
 app.post("/", (req, res) => {
-    if (req.headers.authorization !== process.env.SECRET) return res.status(401).send("Unauthorized");
-    const t = new Date().getTime();
-    const file = fs.createWriteStream(`${process.env.DATA}/${t}`, {flags: "a"});
-    req.on("data", chunk => file.write(chunk));
-    req.on("end", () => {
-        file.end();
-        res.status(200).send("Success");
-    });
+    try {
+        if (req.headers.authorization !== process.env.SECRET) return res.status(401).send("Unauthorized");
+
+        let data = "";
+        req.on("data", chunk => data += chunk);
+
+        req.on("end", () => {
+            fs.writeFileSync(`${process.env.DATA}/${new Date().getTime()}`, Buffer.from(data, "base64").toString("binary"));
+            res.status(200).send("Success");
+        });
+    } catch (e) {
+        res.status(500).send("Internal Error");
+    }
 });
