71c604cd58da6d3d8bc9db21e1c95b323653a196
Archie Baer <archie@abaer.dev>
2020-06-23 21:55:48 +0100

    Get customer by id

---
+++ utils/getUserByCustomerId.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+module.exports = async customerId => {
+	try {
+		return (await axios.get(
+			`https://1api.alles.cx/v1/stripeCustomer?id=${encodeURIComponent(
+				customerId
+			)}`,
+			{
+				auth: {
+					username: process.env.ALLES_ID,
+					password: process.env.ALLES_SECRET
+				}
+			}
+		)).data.id;
+	} catch (e) {
+		console.log(e);
+		return null;
+	}
+};
