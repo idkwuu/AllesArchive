d6e05a26bb0609e7ca29c4f4735c98ee0dbed542
Archie Baer <archie@abaer.dev>
2020-06-23 22:35:48 +0100

    Subscription cancellation

--- events/customer.subscription.deleted.js
+++ events/customer.subscription.deleted.js
@@ -1,1 +1,13 @@
-module.exports = body => {};
+const stripeIds = require("../stripeIds");
+const setPlus = require("../actions/setPlus");
+
+module.exports = async body => {
+	const {customer, plan} = body.data.object;
+	const {product} = plan;
+
+	// Alles+
+	if (stripeIds.plus.includes(product)) await setPlus(customer, false, false);
+	// Alles+ Max
+	else if (stripeIds.plusMax.includes(product))
+		await setPlus(customer, true, false);
+};
