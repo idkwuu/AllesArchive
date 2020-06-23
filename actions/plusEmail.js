8230771675e94737db5f099bcc6afe2417bd8141
Archie Baer <archie@abaer.dev>
2020-06-24 00:02:47 +0100

    Authenticates with pico api key

--- actions/plusEmail.js
+++ actions/plusEmail.js
@@ -4,16 +4,24 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const axios = require("axios");
 
 module.exports = async customerId => {
-    const user = await getUserData(await getUserByCustomerId(customerId));
-    const email = await stripe.customers.retrieve(customerId);
+	const user = await getUserData(await getUserByCustomerId(customerId));
+	const email = await stripe.customers.retrieve(customerId);
 
-    try {
-        axios.post("https://pico.alles.cx/api/v1/send/message", {
-            to: email,
-            from: "plus@alles.cx",
-            reply_to: "archie@alles.cx",
-            subject: "Welcome to Alles+!",
-            html_body: "test"
-        });
-    } catch (e) {}
+	try {
+		axios.post(
+			"https://pico.alles.cx/api/v1/send/message",
+			{
+				to: email,
+				from: "plus@alles.cx",
+				reply_to: "archie@alles.cx",
+				subject: "Welcome to Alles+!",
+				html_body: "test"
+			},
+			{
+				headers: {
+					"X-Server-API-Key": process.env.PICO_KEY
+				}
+			}
+		);
+	} catch (e) {}
 };
