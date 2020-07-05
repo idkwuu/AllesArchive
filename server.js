60f76587d3892efd4897ff0515dedca2ae145b7f
Dante <dante@issaias.net>
2020-07-05 23:16:10 +0100

    Make server.js work in prod with proxy for now

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const devProxy = {
   "/api": {
     target: "https://alles.cx/api/",
     pathRewrite: { "^/api": "/" },
-    changeOrigin: true
-  }
+    changeOrigin: true,
+  },
 };
 
 const port = parseInt(process.env.PORT, 10) || 3000;
@@ -14,7 +14,7 @@ const env = process.env.NODE_ENV;
 const dev = env !== "production";
 const app = next({
   dir: ".", // base directory where everything is, could move to src later
-  dev
+  dev,
 });
 
 const handle = app.getRequestHandler();
@@ -26,24 +26,22 @@ app
     server = express();
 
     // Set up the proxy.
-    if (dev && devProxy) {
-      const { createProxyMiddleware } = require("http-proxy-middleware");
-      Object.keys(devProxy).forEach(function(context) {
-        server.use(createProxyMiddleware(context, devProxy[context]));
-      });
-    }
+    const { createProxyMiddleware } = require("http-proxy-middleware");
+    Object.keys(devProxy).forEach(function (context) {
+      server.use(createProxyMiddleware(context, devProxy[context]));
+    });
 
     // Default catch-all handler to allow Next.js to handle all other routes
     server.all("*", (req, res) => handle(req, res));
 
-    server.listen(port, err => {
+    server.listen(port, (err) => {
       if (err) {
         throw err;
       }
       console.log(`> Ready on port ${port} [${env}]`);
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("An error occurred, unable to start the server");
     console.log(err);
   });
