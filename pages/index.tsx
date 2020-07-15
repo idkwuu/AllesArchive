647fa4031c47fe3b8f74122a83ec5062d7b8b166
Dante <dante@issaias.net>
2020-07-15 17:31:05 +0100

    Move header to custom app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,5 @@
-import {
-  Header,
-  Breadcrumb,
-  Avatar,
-  Box,
-  Popover,
-  Transition,
-  Menu,
-} from "@reactants/ui";
-import { Circle, User, Shield, Bell, Icon } from "react-feather";
+import { Box } from "@reactants/ui";
+import { User, Shield, Icon } from "react-feather";
 import { Fragment } from "react";
 
 interface Category {
@@ -45,86 +37,35 @@ const categories: Category[] = [
 
 export default () => {
   return (
-    <>
-      <Header>
-        <div className="p-5 max-w-2xl w-full mx-auto flex justify-between">
-          <Breadcrumb>
-            <Breadcrumb.Item
-              href="#"
-              className="font-medium text-lg inline-flex items-center"
-            >
-              <Circle className="text-gray-500 inline w-5 mr-2" />
-              Alles
-            </Breadcrumb.Item>
-          </Breadcrumb>
+    <main className="sm:max-w-2xl p-5 mx-auto my-5 space-y-7">
+      <h4 className="font-medium text-3xl">
+        Hey, Dante<sup className="select-none text-primary">+</sup>
+      </h4>
 
-          <div className="flex items-center space-x-3">
-            <div className="select-none cursor-pointer hover:bg-danger-85 transition duration-200 ease bg-danger text-white rounded-full flex items-center justify-center py-0.5 px-2.5 space-x-1">
-              <Bell size={0.35 * 37.5} />
-              <span>2</span>
-            </div>
+      <div className="flex space-x-3">
+        {categories.map((category, i) => (
+          <Box className="flex-1" key={i}>
+            <Box.Header className="flex items-center space-x-2">
+              <category.icon size={18.5} className="text-primary" />
+              <span>{category.name}</span>
+            </Box.Header>
 
-            <Popover
-              className="relative inline-block"
-              trigger={(onClick) => (
-                <Avatar
-                  onClick={onClick}
-                  className="select-none cursor-pointer hover:opacity-85 transition duration-200 ease"
-                  username="dante"
-                  size={35}
-                />
-              )}
-              content={(isOpen) => (
-                <Transition
-                  show={isOpen}
-                  enter="transition ease-out duration-100 transform"
-                  enterFrom="opacity-0 scale-95"
-                  enterTo="opacity-100 scale-100"
-                  leave="transition ease-in duration-75 transform"
-                  leaveFrom="opacity-100 scale-100"
-                  leaveTo="opacity-0 scale-95"
-                >
-                  <Menu className="absolute origin-top-right right-0">
-                    <Menu.Item>Settings</Menu.Item>
-                    <Menu.Item>Log out</Menu.Item>
-                  </Menu>
-                </Transition>
-              )}
-            />
-          </div>
-        </div>
-      </Header>
-
-      <main className="sm:max-w-2xl p-5 mx-auto my-5 space-y-7">
-        <h4 className="font-medium text-3xl">
-          Hey, Dante<sup className="select-none text-primary">+</sup>
-        </h4>
-
-        <div className="flex space-x-3">
-          {categories.map((category, i) => (
-            <Box className="flex-1" key={i}>
-              <Box.Header className="flex items-center space-x-2">
-                <category.icon size={18.5} className="text-primary" />
-                <span>{category.name}</span>
-              </Box.Header>
-
-              {category.links.map((link, i) => (
-                <Fragment key={i}>
-                  {i >= 1 && <hr className="opacity-25" />}
-                  <Box.Content>
-                    <a
-                      className="hover:text-primary transition duration-200 ease"
-                      href={link.href}
-                    >
-                      {link.text}
-                    </a>
-                  </Box.Content>
-                </Fragment>
-              ))}
-            </Box>
-          ))}
-        </div>
-      </main>
-    </>
+            {category.links.map((link, i) => (
+              <Fragment key={i}>
+                {i >= 1 && <hr className="opacity-25" />}
+                <Box.Content>
+                  <a
+                    className="hover:text-primary transition duration-200 ease"
+                    href={link.href}
+                  >
+                    {link.text}
+                  </a>
+                </Box.Content>
+              </Fragment>
+            ))}
+          </Box>
+        ))}
+      </div>
+    </main>
   );
 };
