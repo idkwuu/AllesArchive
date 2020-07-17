753ac45350e1215a09100b7fa9fa0ef98a9f76a4
Archie Baer <archie@abaer.dev>
2020-07-17 21:01:37 +0100

    User model

---
+++ db/User.js
@@ -0,0 +1,58 @@
+const {DataTypes} = require("sequelize");
+
+module.exports = db => {
+	db.User = db.define(
+		"user",
+		{
+			id: {
+				primaryKey: true,
+				type: DataTypes.UUID,
+				allowNull: false
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			nickname: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			tag: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			password: {
+				type: DataTypes.STRING
+			},
+			reputation: {
+				type: DataTypes.INTEGER,
+				defaultValue: 0,
+				allowNull: false
+			},
+            country: {
+                type: DataTypes.STRING
+			},
+			bDay: {
+				type: DataTypes.TINYINT
+			},
+			bMonth: {
+				type: DataTypes.TINYINT
+			},
+			bYear: {
+				type: DataTypes.SMALLINT
+			},
+			plus: {
+				type: DataTypes.BOOLEAN,
+				defaultValue: false,
+				allowNull: false
+			},
+			stripeCustomerId: {
+				type: DataTypes.STRING
+			}
+		},
+		{
+			updatedAt: false,
+			paranoid: true
+		}
+	);
+};
