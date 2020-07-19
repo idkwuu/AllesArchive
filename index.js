06e173f155f1e6d343d2c3aacab56994b2ebe7da
Archie Baer <archie@abaer.dev>
2020-07-19 13:16:56 +0100

    Syncs db

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express();
 app.use(require("body-parser").json());
 app.use(require("./auth"));
 app.use((err, req, res, next) => res.status(500).json({err: "internalError"}));
-app.listen(8080, () => console.log("Express is listening..."));
+
+// Database
+const db = require("./db");
+db.sync().then(() =>
+	app.listen(8080, () => console.log("Server is listening..."))
+);
 
 // 404
 app.use((req, res) => res.status(404).json({err: "notFound"}));
