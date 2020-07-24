431c5119ed2693ee1d4013070a52e3b6d3f2fd91
Archie Baer <archie@abaer.dev>
2020-07-24 19:01:00 +0100

    Docker

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const nexus = require("@alleshq/nexus");
+// Express
 const express = require("express");
 const app = express();
 app.listen(8080, () => console.log("Server is online!"));
+
+// Nexus
+const nexus = require("@alleshq/nexus");
+nexus.setCredentials(process.env.NEXUS_ID, process.env.NEXUS_SECRET);
 
 // 404
 app.use((req, res) => res.status(404).json({err: "notFound"}));
