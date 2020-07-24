107153288893ac40915e9df295f85e3baae2fde4
Archie Baer <archie@abaer.dev>
2020-07-24 18:20:01 +0100

    404 route

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const nexus = require("@alleshq/nexus");
 const express = require("express");
 const app = express();
 app.listen(8080, () => console.log("Server is online!"));
+
+// 404
+app.use((req, res) => res.status(404).json({err: "notFound"}));
