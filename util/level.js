a6e45b6ad767676493ba506f1593fc3edbdf91b4
Archie Baer <archie@abaer.dev>
2020-07-27 20:58:21 +0100

    Formatting

--- util/level.js
+++ util/level.js
@@ -1,15 +1,15 @@
 module.exports = xp => {
-    let level = 1;
-    let levelMaxXp = 1000;
-    let remainingXp = xp;
-    while (remainingXp >= levelMaxXp) {
-        level++;
-        remainingXp -= levelMaxXp;
-        levelMaxXp += 100;
-    }
-    return {
-        level,
-        levelMaxXp,
-        remainingXp
-    };
+	let level = 1;
+	let levelMaxXp = 1000;
+	let remainingXp = xp;
+	while (remainingXp >= levelMaxXp) {
+		level++;
+		remainingXp -= levelMaxXp;
+		levelMaxXp += 100;
+	}
+	return {
+		level,
+		levelMaxXp,
+		remainingXp
+	};
 };
