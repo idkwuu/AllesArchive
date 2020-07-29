f62843ca6edfdb6e2ea436b928edcf1f25a852ba
Archie Baer <archie@abaer.dev>
2020-07-30 00:09:58 +0100

    Removed cache header

--- api/[id].js
+++ api/[id].js
@@ -4,7 +4,6 @@ const errorCard = require("../errorCard.js");
 
 module.exports = (req, res) => {
     res.setHeader("Content-Type", "image/svg+xml");
-    res.setHeader("Cache-Control", "public, max-age=86400");
     axios.get(`https://horizon.alles.cc/users/${encodeURIComponent(req.query.id)}`)
         .then(({data}) => res.send(renderCard(data)))
         .catch(() => res.status(404).send(errorCard));
