ed48c497ac2f6c75ee577780659de974fb6d5822
Archie Baer <archie@abaer.dev>
2020-07-29 16:34:41 +0100

    Error card

--- api/[id].js
+++ api/[id].js
@@ -1,11 +1,8 @@
 const axios = require("axios");
+const renderCard = require("../renderCard.js");
+const errorCard = require("../errorCard.js");
 
 module.exports = (req, res) =>
     axios.get(`https://horizon.alles.cc/users/${encodeURIComponent(req.query.id)}`)
-        .then(({data}) => {
-            console.log(data);
-            res.send("Something went right!");
-        })
-        .catch(() => {
-            res.status(400).send("Something went wrong!");
-        });
+        .then(({data}) => res.send(renderCard(data)))
+        .catch(() => res.status(400).send(errorCard));
---
+++ errorCard.js
@@ -0,0 +1,15 @@
+module.exports = `
+<svg width="495" height="120" viewBox="0 0 495 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <style>
+        .text { font: 600 16px "Segoe UI", Ubuntu, Sans-Serif; fill: #2F80ED }
+        .small { font: 600 12px "Segoe UI", Ubuntu, Sans-Serif; fill: #252525 }
+        .gray { fill: #858585 }
+    </style>
+    <rect x="0.5" y="0.5" width="494" height="99%" rx="4.5" fill="#FFFEFE" stroke="#E4E2E2"/>
+    <text x="25" y="45" class="text">Something went wrong!</text>
+    <text data-testid="message" x="25" y="55" class="text small">
+        <tspan x="25" dy="18">Make sure that the user id is correct</tspan>
+        <tspan x="25" dy="18"  class="gray">Find your id at https://alles.link/get-id</tspan>
+    </text>
+</svg>
+`;
