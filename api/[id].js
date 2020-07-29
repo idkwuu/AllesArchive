984b6ee386cf46b0b690ac40e1cd3792db811bc7
Archie Baer <archie@abaer.dev>
2020-07-30 00:05:07 +0100

    Headers

--- api/[id].js
+++ api/[id].js
@@ -2,7 +2,10 @@ const axios = require("axios");
 const renderCard = require("../renderCard.js");
 const errorCard = require("../errorCard.js");
 
-module.exports = (req, res) =>
+module.exports = (req, res) => {
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("Cache-Control", "public, max-age=86400");
     axios.get(`https://horizon.alles.cc/users/${encodeURIComponent(req.query.id)}`)
         .then(({data}) => res.send(renderCard(data)))
         .catch(() => res.status(404).send(errorCard));
+}
