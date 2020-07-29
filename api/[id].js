cf3871c4b313604d1282c10a49335cf8953ad0dc
Archie Baer <archie@abaer.dev>
2020-07-29 20:49:51 +0100

    Changed status code to 404

--- api/[id].js
+++ api/[id].js
@@ -5,4 +5,4 @@ const errorCard = require("../errorCard.js");
 module.exports = (req, res) =>
     axios.get(`https://horizon.alles.cc/users/${encodeURIComponent(req.query.id)}`)
         .then(({data}) => res.send(renderCard(data)))
-        .catch(() => res.status(400).send(errorCard));
+        .catch(() => res.status(404).send(errorCard));
