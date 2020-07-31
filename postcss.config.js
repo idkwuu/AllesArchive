248b47fc6ec56bce02130c7c797abefb4aeca228
Dante <dante@issaias.net>
2020-07-31 12:36:49 -0700

    Src folder

--- postcss.config.js
+++ postcss.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  plugins:
-    process.env.NODE_ENV === "production"
-      ? [
-          [
-            "@fullhuman/postcss-purgecss",
-            {
-              defaultExtractor: (content) => {
-                // Capture as liberally as possible, including things like `h-(screen-1.5)`
-                const broadMatches =
-                  content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+	plugins:
+		process.env.NODE_ENV === "production"
+			? [
+					[
+						"@fullhuman/postcss-purgecss",
+						{
+							defaultExtractor: content => {
+								// Capture as liberally as possible, including things like `h-(screen-1.5)`
+								const broadMatches =
+									content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
 
-                // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-                const innerMatches =
-                  content.match(
-                    /[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g
-                  ) || [];
+								// Capture classes within other delimiters like .block(class="w-1/2") in Pug
+								const innerMatches =
+									content.match(
+										/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g
+									) || [];
 
-                return broadMatches.concat(innerMatches);
-              },
-              whitelist: ["dark", "html", "body"],
-              content: [
-                "./pages/**/*.tsx",
-                "./components/**/*.tsx",
-                "./node_modules/@reactants/ui/dist/index.js",
-              ],
-            },
-          ],
-          "postcss-preset-env",
-        ]
-      : ["postcss-preset-env"],
+								return broadMatches.concat(innerMatches);
+							},
+							whitelist: ["dark", "html", "body"],
+							content: [
+								"./pages/**/*.tsx",
+								"./components/**/*.tsx",
+								"./node_modules/@reactants/ui/dist/index.js"
+							]
+						}
+					],
+					"postcss-preset-env"
+			  ]
+			: ["postcss-preset-env"]
 };
