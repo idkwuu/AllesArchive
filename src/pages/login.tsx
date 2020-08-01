4a5e45488f2c51e7e2dfbd141337022493f304a3
Dante Issaias <dante@issaias.net>
2020-08-01 21:53:46 +0100

    Some basic nametag validation messages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,6 +9,8 @@ export default function Login() {
 	const [password, setPassword] = useState<string>("");
 	const [loading, setLoading] = useState<boolean>(false);
 	const [error, setError] = useState<string>("");
+	const [nametagError, setNametagError] = useState<string>("");
+	const [passwordError, setPasswordError] = useState<string>("");
 
 	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -40,6 +42,21 @@ export default function Login() {
 			});
 	};
 
+	const REQUIRED_FIELD = (field: string) => `${field} is a required field.`;
+	const INVALID_FIELD = (field: string) => `${field} is invalid or malformed.`;
+
+	const validateNametag = ({ target }: React.FocusEvent<HTMLInputElement>) => {
+		if (target.value === "") setNametagError(REQUIRED_FIELD("Nametag"));
+		else if (!target.value.includes("#"))
+			setNametagError(INVALID_FIELD("Nametag"));
+		else setNametagError("");
+	};
+
+	const updateNametag = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNametag(e.target.value);
+		validateNametag({ target: e.target } as any);
+	};
+
 	return (
 		<main className="sm:max-w-sm p-5 mx-auto mt-12 space-y-7">
 			<h1 className="font-medium text-center mb-5 text-4xl">Sign In</h1>
@@ -47,21 +64,29 @@ export default function Login() {
 			<Box>
 				<Box.Header>Enter your credentials</Box.Header>
 				<Box.Content className="px-5 py-6">
-					<form method="POST" onSubmit={onSubmit} className="space-y-5">
-						<Input
-							label="Nametag"
-							value={nametag}
-							onChange={e => setNametag(e.target.value)}
-							placeholder="Jessica Adams#0001"
-						/>
+					<form method="POST" onSubmit={onSubmit} className="space-y-7">
+						<div className="space-y-2">
+							<Input
+								label="Nametag"
+								value={nametag}
+								onChange={e => updateNametag(e)}
+								placeholder="Jessica Adams#0001"
+								errored={!!nametagError}
+								note={nametagError}
+								onBlur={validateNametag}
+								hasNote={true}
+							/>
 
-						<Input
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-							label="Password"
-							type="password"
-							placeholder="••••••••••"
-						/>
+							<Input
+								value={password}
+								onChange={e => setPassword(e.target.value)}
+								label="Password"
+								type="password"
+								placeholder="••••••••••"
+								errored={!!passwordError}
+								note={passwordError}
+							/>
+						</div>
 
 						<Button
 							loading={loading}
