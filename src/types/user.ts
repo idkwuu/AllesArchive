fdc28f78ec7e2ef9b503ce872640fef1e2142edb
Dante Issaias <dante@issaias.net>
2020-08-02 03:45:02 +0100

    Add missing fields to types

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,4 +2,19 @@ export type User = {
 	id: string;
 	name: string;
 	tag: string;
+	nickname: string;
+	plus: boolean;
+	createdAt: string;
+	reputation: number;
+	xp: {
+		total: number;
+		level: number;
+		levelXp: number;
+		levelXpMax: number;
+		levelProgress: number;
+	};
+	hasPassword: boolean;
+	stripeCustomerId: string;
+	country: string;
+	birth: { day: number; month: number; year: number; date: string };
 };
