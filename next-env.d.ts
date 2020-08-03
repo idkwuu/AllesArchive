40d52272de597561918e69daa949a2f9b5c0b011
Dante Issaias <dante@issaias.net>
2020-08-03 17:41:20 +0100

    Add typings for environment variables

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+// Extend the NodeJS namespace with Next.js-defined properties
+declare namespace NodeJS {
+	interface ProcessEnv {
+		readonly NODE_ENV: "development" | "production" | "test";
+		readonly NEXUS_ID: string;
+		readonly NEXUS_SECRET: string;
+		readonly NEXUS_URI: string;
+		readonly COOKIE_DOMAIN: string;
+		readonly PUBLIC_URI: string;
+	}
+}
