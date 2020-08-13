9be126a8d8031ca8762a69d3b8b49e27b34c8dc4
Archie Baer <archie@abaer.dev>
2020-08-13 19:40:17 +0100

    Fixed Nexus url bug

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ app.get("/nametag", (req, res) => {
     if (typeof req.query.name !== "string" || typeof req.query.tag !== "string")
         return res.status(400).json({err: "badRequest"});
     
-    nexus("GET", `${process.env.NEXUS_URI}/nametag?name=${encodeURIComponent(req.query.name)}&tag=${encodeURIComponent(req.query.id)}`)
+    nexus("GET", `nametag?name=${encodeURIComponent(req.query.name)}&tag=${encodeURIComponent(req.query.id)}`)
         .then(({id}) => res.json({id}))
         .catch(() => res.status(404).json({err: "missingResource"}));
 });
 
 // Get user data
 app.get("/users/:id", (req, res) => {
-    nexus("GET", `${process.env.NEXUS_URI}/users/${encodeURIComponent(req.params.id)}`)
+    nexus("GET", `users/${encodeURIComponent(req.params.id)}`)
         .then(user => res.json({
             id: user.id,
             name: user.name,
