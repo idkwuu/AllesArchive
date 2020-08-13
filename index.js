ca9aec64ec20f21bd59485bc013796b650cbc93f
Archie Baer <archie@abaer.dev>
2020-08-13 17:54:59 +0100

    Updated Nexus

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ const app = express();
 app.listen(8080, () => console.log("Server is online!"));
 
 // Nexus
-const nexus = require("@alleshq/nexus");
-nexus.setCredentials(process.env.NEXUS_ID, process.env.NEXUS_SECRET);
+const axios = require("axios");
+const nexus = async (method, endpoint, data) => (await axios({
+    method,
+    url: `${process.env.NEXUS_URI}/${endpoint}`,
+    data,
+    auth: {
+        username: process.env.NEXUS_ID,
+        password: process.env.NEXUS_SECRET
+    }
+})).data;
 
 // Get User ID from Name#tag
 app.get("/nametag", (req, res) => {
     if (typeof req.query.name !== "string" || typeof req.query.tag !== "string")
         return res.status(400).json({err: "badRequest"});
     
-    nexus.nametag(req.query.name, req.query.tag)
+    nexus("GET", `${process.env.NEXUS_URI}/nametag?name=${encodeURIComponent(req.query.name)}&tag=${encodeURIComponent(req.query.id)}`)
         .then(({id}) => res.json({id}))
         .catch(() => res.status(404).json({err: "missingResource"}));
 });
 
 // Get user data
 app.get("/users/:id", (req, res) => {
-    nexus.getUser(req.params.id)
+    nexus("GET", `${process.env.NEXUS_URI}/users/${encodeURIComponent(req.params.id)}`)
         .then(user => res.json({
             id: user.id,
             name: user.name,
