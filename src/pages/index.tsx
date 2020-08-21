fa641c04d60f579649f5fd1d15b393c556329bfe
Dante Issaias <dante@issaias.net>
2020-08-21 17:56:42 +0100

    Redesign app tiles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Box } from "@reactants/ui";
 import { User as UserIcon, Shield, Icon, Grid, EyeOff } from "react-feather";
-import { Fragment } from "react";
 import Link from "next/link";
 import { Page } from "../components";
 import { useUser } from "../lib";
@@ -13,7 +12,7 @@ interface Category {
 
 interface Product {
 	name: string;
-	image: string;
+	logo: React.ReactNode;
 	url: string;
 }
 
@@ -58,46 +57,47 @@ const categories: Category[] = [
 	},
 ];
 
+const Micro = (
+	<div className="pb-2 font-bold font-serif italic text-4xl text-primary rounded-t-lg">
+		μ
+	</div>
+);
+
+const Spectare = (
+	<div className="flex w-full items-centers justify-center">
+		<svg
+			width="62"
+			height="62"
+			viewBox="0 0 128 128"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M69.5542 47.3936C68.4069 47.3936 67.4786 48.6667 67.4786 50.2375C67.4786 51.8102 68.407 53.083 69.5542 53.083C70.7013 53.083 71.6289 51.8102 71.6289 50.2375C71.629 48.6667 70.7013 47.3936 69.5542 47.3936Z"
+				fill="#23539e"
+			/>
+			<path
+				d="M69.9675 33C59.4938 33 51.0012 43.1013 51.0012 55.5595C51.0012 67.3227 51.0441 76.6716 39.4088 92.2307C39.0073 92.7655 38.951 93.4812 39.2628 94.0709C39.5707 94.6624 40.1934 95.0235 40.86 94.9989C81.0918 93.5254 88.9371 67.663 88.9371 55.5595C88.9371 43.1013 80.4452 33 69.9675 33ZM68.7331 57.7258C66.2075 57.7258 64.163 54.4647 64.163 50.4439C64.163 46.423 66.2075 43.1622 68.7331 43.1622C71.2559 43.1622 73.3031 46.423 73.3031 50.4439C73.303 54.4647 71.2559 57.7258 68.7331 57.7258ZM79.8883 57.7258C77.3654 57.7258 75.3183 54.4647 75.3183 50.4439C75.3183 46.423 77.3654 43.1622 79.8883 43.1622C82.4138 43.1622 84.4583 46.423 84.4583 50.4439C84.4583 54.4647 82.4137 57.7258 79.8883 57.7258Z"
+				fill="#23539e"
+			/>
+			<path
+				d="M79.0671 47.3936C77.9198 47.3936 76.9924 48.6667 76.9924 50.2375C76.9924 51.8102 77.92 53.083 79.0671 53.083C80.2145 53.083 81.1427 51.8102 81.1427 50.2375C81.1426 48.6667 80.2144 47.3936 79.0671 47.3936Z"
+				fill="#23539e"
+			/>
+		</svg>
+	</div>
+);
+
 const products: Product[] = [
 	{
 		name: "Micro",
-		image: "micro",
+		logo: Micro,
 		url: "https://micro.alles.cx",
 	},
 	{
-		name: "Micro",
-		image: "micro",
-		url: "https://micro.alles.cx",
-	},
-	{
-		name: "Micro",
-		image: "micro",
-		url: "https://micro.alles.cx",
-	},
-	{
-		name: "Micro",
-		image: "micro",
-		url: "https://micro.alles.cx",
-	},
-	{
-		name: "Micro",
-		image: "micro",
-		url: "https://micro.alles.cx",
-	},
-	{
-		name: "Micro",
-		image: "micro",
-		url: "https://micro.alles.cx",
-	},
-	{
-		name: "Micro",
-		image: "micro",
-		url: "https://micro.alles.cx",
-	},
-	{
-		name: "Micro",
-		image: "micro",
-		url: "https://micro.alles.cx",
+		name: "Spectare",
+		logo: Spectare,
+		url: "https://spectare.alles.cx",
 	},
 ];
 
@@ -112,17 +112,14 @@ export default function Index() {
 					{user.plus && <sup className="select-none text-primary">+</sup>}
 				</h4>
 
-				<div className="grid grid-cols-2 md:grid-cols-4 gap-3">
+				<div className="grid grid-cols-2 md:grid-cols-6 gap-3">
 					{products.map((product, i) => (
 						<a href={product.url} key={i}>
-							<Box>
-								<Box.Content className="flex">
-									<img
-										className="w-10 h-10 rounded-lg mr-3"
-										src={`/productIcons/${product.image}.svg`}
-									/>
-									<span className="my-auto">{product.name}</span>
-								</Box.Content>
+							<Box className="hover:shadow-lg transition duration-100 ease text-center">
+								{product.logo}
+								<div className="rounded-b-lg py-1.5 w-full border-t text-sm text-gray-500 dark:text-gray-300 border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-750">
+									{product.name}
+								</div>
 							</Box>
 						</a>
 					))}
