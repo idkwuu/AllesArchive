f4ec07a3905c01b43b6736ce7189c4c602cd2f38
Archie Baer <archie@abaer.dev>
2020-08-28 00:37:06 +0100

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+import { Page } from "../components/page";
+import { ConfusedCat } from "../components/confusedCat";
+
+export default function NotFound() {
+	return (
+		<Page>
+			<main className="sm:max-w-2xl p-5 mx-auto space-y-7">
+				<h1 className="text-5xl font-medium">404</h1>
+				<p>
+					We're having trouble finding that page! Maybe go to the{" "}
+					<a className="text-primary" href="/">
+						homepage
+					</a>
+					.
+				</p>
+
+				<ConfusedCat />
+				<p className="text-right text-sm">
+					Confused Alles Cat by{" "}
+					<a
+						className="text-primary"
+						href="https://micro.alles.cx/93e0b06c-e129-415a-aa0a-476473f4d0d2"
+					>
+						Will Jones
+					</a>
+				</p>
+			</main>
+		</Page>
+	);
+}
