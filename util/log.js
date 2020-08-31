16d3f506d44e4b08dbe3cf85e9205fc014e9f07b
Archie Baer <archie@abaer.dev>
2020-08-31 01:40:55 +0100

    This isn't working...

--- util/log.js
+++ util/log.js
@@ -2,38 +2,24 @@ const db = require("../db");
 const uuid = require("uuid").v4;
 
 module.exports = async (name, params, clientId, userId) => {
-  // Create Event
-  const event = await db.Event.create({
-    id: uuid(),
-    name,
-  });
+  try {
+    const event = await db.Event.create({
+      id: uuid(),
+      name,
+      clientId,
+      userId,
+    });
 
-  // Client Association
-  const client = await db.Client.findOne({
-    where: {
-      id: clientId,
-    },
-  });
-  await event.setClient(client);
-
-  // User Association
-  const user = await db.User.findOne({
-    where: {
-      id: userId,
-    },
-  });
-  await event.setUser(user);
-
-  // Create Parameters
-  if (params)
-    await Promise.all(
-      Object.keys(params).map((key) =>
-        db.EventParam.create({
-          id: uuid(),
-          key,
-          value: params[key],
-          eventId: event.id,
-        })
-      )
-    );
+    if (params)
+      await Promise.all(
+        Object.keys(params).map((key) =>
+          db.EventParam.create({
+            id: uuid(),
+            key,
+            value: params[key],
+            eventId: event.id,
+          })
+        )
+      );
+  } catch (err) {}
 };
