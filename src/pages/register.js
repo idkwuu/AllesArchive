4f1bb5532b5495b3b8de1ad5b8fba21b46e4be49
Archie Baer <archie@abaer.dev>
2020-09-01 17:42:54 +0100

    Register page placeholder

---
+++ src/pages/register.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import {
+	Box,
+	Input,
+	Button,
+	Toast,
+	Breadcrumb,
+	Transition,
+} from "@alleshq/reactants";
+import { LogIn, Circle } from "react-feather";
+import { Page } from "../components/page";
+
+export default function Register() {
+	return (
+		<Page
+			authenticated={false}
+			title="Sign up"
+			breadcrumbs={
+				<Breadcrumb.Item className="opacity-75 pointer-events-none select-none">
+					Sign up
+				</Breadcrumb.Item>
+			}
+		>
+			<main className="sm:max-w-sm p-5 mx-auto space-y-7">
+				<h1 className="font-medium text-center mb-5 text-4xl">Join Alles!</h1>
+
+				<Box>
+					<Box.Content className="px-5 py-6">
+						<p>
+							Sorry, you can't make an account here just yet, but you can ask
+							and if we're feeling nice, we'll probably make you one!
+						</p>
+						<div className="space-y-2 mt-5">
+							<Button
+								size="md"
+								className="w-full"
+								onClick={() => (location.href = "https://alles.link/discord")}
+							>
+								Join our Discord
+							</Button>
+							<Button
+								size="md"
+								className="w-full"
+								onClick={() => (location.href = "https://alles.link/twitterdm")}
+							>
+								DM us on Twitter
+							</Button>
+						</div>
+					</Box.Content>
+				</Box>
+			</main>
+		</Page>
+	);
+}
