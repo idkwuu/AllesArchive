7590be186a83fd2bc9535cffdebd663c360a986d
Archie Baer <archie@abaer.dev>
2020-09-05 14:38:35 +0100

    Changed player colors

--- api/join.js
+++ api/join.js
@@ -10,7 +10,11 @@ module.exports = async (req, res) => {
         name: `${req.user.name}#${req.user.tag}`,
         score: 100,
         secret,
-        color: req.user.plus ? "#4287f5" : "#e74c3c",
+        color: req.user.id === "00000000-0000-0000-0000-000000000000" ?
+            "#8b0dd4" :
+            req.user.plus ?
+                "#f7c614" :
+                "#e74c3c",
         plague: Math.floor(Math.random() * 5) === 0,
         bulletPower: 2,
         speed: 2,
