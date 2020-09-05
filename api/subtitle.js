c58bee72361e8619abeaf0778c15f7dd5e2283db
Archie Baer <archie@abaer.dev>
2020-09-05 14:19:44 +0100

    Updated splash texts

--- api/subtitle.js
+++ api/subtitle.js
@@ -3,10 +3,12 @@ const subtitles = require("../subtitles");
 module.exports = async (req, res) => {
     res.send(
         (
-            req.user.id !== "c1d1fe95-cac7-4d92-8cd8-8f30d6ac5ca8" &&
+            req.user.id === "c1d1fe95-cac7-4d92-8cd8-8f30d6ac5ca8" &&
             Math.floor(Math.random() * 2) === 0
         ) ?
             "#BringBackBdrian" :
-            subtitles[Math.floor(Math.random() * subtitles.length)]
+            Math.floor(Math.random() * 5) === 0 ?
+                "#BlackLivesMatter" :
+                subtitles[Math.floor(Math.random() * subtitles.length)]
     );
 };
