548a33fb8f2f97323c3fd99c312a077657a19639
Archie Baer <archie@abaer.dev>
2020-09-12 23:48:43 +0100

    Logs user id

--- events/customer.subscription.created.js
+++ events/customer.subscription.created.js
@@ -7,6 +7,7 @@ module.exports = async event => {
   const { product } = plan;
   const customer = await stripe.customers.retrieve(customerId);
   const { userId } = customer.metadata;
+  console.log(userId);
 
   // Alles+
   if (
