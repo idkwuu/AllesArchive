8510fb726f28745764aa3f4a4403bd0e48b38d6b
Archie Baer <archie@abaer.dev>
2020-10-11 00:26:30 +0100

    CORS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 // Express
 const express = require("express");
 const app = express();
+app.use(require("cors")());
 app.use(require("body-parser").json());
 app.use((_err, _req, res, _next) => res.status(500).json({err: "internalError"}));
 app.listen(8080, () => console.log("Server is listening..."));
