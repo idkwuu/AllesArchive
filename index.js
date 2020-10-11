293b4756bf886a375cd7c0c31d2ed236b59838d6
Archie Baer <archie@abaer.dev>
2020-10-11 17:29:06 +0100

    Avoided everyone username ping

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ const userStats = async (id, msg) => {
         `**Name:** ${esc(user.name)}\n` +
         `**Tag:** #${user.tag}\n` +
         `**Nickname:** ${esc(user.nickname)}\n` +
-        (user.username ? `**Username:** @${esc(user.username)}\n` : ``) +
+        (user.username ? `**Username:** ${esc(`@${user.username}`)}\n` : ``) +
         `**XP:** ${user.xp.total} (Level ${user.xp.level}, ${user.xp.levelXp}/${user.xp.levelXpMax})\n` +
         `${"▓".repeat(progress)}${"░".repeat(10 - progress)} ${Math.floor(user.xp.levelProgress * 100)}%` +
         `\n\nhttps://people.alles.cx/${encodeURIComponent(user.username ? user.username : user.id)}` +
