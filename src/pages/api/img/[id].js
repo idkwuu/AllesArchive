5e540c6d81380fb655f5fc5eb1d6068ff0c150fe
Archie Baer <archie@abaer.dev>
2020-10-17 04:03:32 +0100

    User profile image generated

--- src/pages/api/img/[id].js
+++ src/pages/api/img/[id].js
@@ -1,22 +1,46 @@
 import getUser from "../../../utils/getUser";
-import capture from "capture-website";
+import sharp from "sharp";
 
 export default async (req, res) => {
   if (typeof req.query.id !== "string")
     return res.status(400).json({ err: "badRequest" });
 
   // Get user
-  const u = await getUser(req.query.id);
-  if (!u) return res.status(404).json({ err: "missingResource" });
+  const user = await getUser(req.query.id);
+  if (!user) return res.status(404).json({ err: "missingResource" });
 
-  // Capture Website
-  try {
-    const img = await capture.buffer(
-      `${process.env.NEXT_PUBLIC_ORIGIN}/${u.id}`
-    );
-    res.setHeader("Content-Type", "image/png");
-    res.send(img);
-  } catch (err) {
-    res.status(500).json({ err: "internalError" });
-  }
+  // Create SVG
+  const svg = `
+    <svg
+      width="500"
+      height="500"
+      viewBox="0 0 500 500"
+      xmlns="http://www.w3.org/2000/svg"
+      font-family="sans serif"
+    >
+      <rect
+        x="0"
+        y="0"
+        width="500"
+        height="500"
+        fill="#ffffff"
+      />
+      <text x="50" y="75" fill="#000000" font-weight="bold" font-size="25">
+        <tspan>${encodeHTML(user.name)}</tspan>
+        <tspan fill="#23539b" font-size="15">#${user.tag}</tspan>
+      </text>
+    </svg>
+  `;
+
+  // Convert to PNG
+  const img = await sharp(Buffer.from(svg)).png();
+
+  // Response
+  res.setHeader("Content-Type", "image/png");
+  res.send(img);
 };
+
+const encodeHTML = (str) =>
+  str
+    .replace(/[\u00A0-\u9999<>&](?!#)/gim, (i) => "&#" + i.charCodeAt(0) + ";")
+    .replace(/\u0008/gim, "");
