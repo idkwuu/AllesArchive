7293fc1269a61ed629ad146e8a5f36fbab69355b
Archie Baer <archie@abaer.dev>
2020-10-18 15:24:55 +0100

    Corrected config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
 	maxNicknameLength: 25,
-	minUsernameLevel: 5,
-	minUsernameLength: 3,
+	minUsernameLevel: 3,
+	minUsernameLength: 5,
 	maxUsernameLength: 25,
 	maxPasswordLength: 256,
 	maxEmailLength: 100,
