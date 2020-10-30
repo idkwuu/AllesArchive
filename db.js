ec602b939997501d49b555a4b489552b7cf5dd6a
Archie Baer <archie@abaer.dev>
2020-10-30 00:13:50 +0000

    Formatting (using Prettier)

--- db.js
+++ db.js
@@ -1,45 +1,49 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: "mariadb",
-        logging: false,
-        dialectOptions: {
-            timezone: "Etc/GMT0"
-        }
-    }
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mariadb",
+    logging: false,
+    dialectOptions: {
+      timezone: "Etc/GMT0",
+    },
+  }
 );
 
-module.exports = db.define("user", {
+module.exports = db.define(
+  "user",
+  {
     id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
     },
     name: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     tag: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     username: {
-        type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     data: {
-        type: Sequelize.TEXT,
-        allowNull: false
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     cachedAt: {
-        type: Sequelize.DATE,
-        allowNull: false
-    }
-}, {
-    timestamps: false
-});
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 db.sync();
--- index.js
+++ index.js
@@ -9,86 +9,93 @@ app.listen(8080, () => console.log("Server is online!"));
 
 // Nexus
 const axios = require("axios");
-const nexus = async (endpoint, data) => (await axios({
-    method: "GET",
-    url: `${process.env.NEXUS_URI}/${endpoint}`,
-    data,
-    auth: {
+const nexus = async (endpoint, data) =>
+  (
+    await axios({
+      method: "GET",
+      url: `${process.env.NEXUS_URI}/${endpoint}`,
+      data,
+      auth: {
         username: process.env.NEXUS_ID,
-        password: process.env.NEXUS_SECRET
-    }
-})).data;
+        password: process.env.NEXUS_SECRET,
+      },
+    })
+  ).data;
 
 // Get User From Nexus (and cache in database)
-const getUserFromNexus = async id => {
-    const user = await nexus(`users/${encodeURIComponent(id)}`);
-    const data = {
-        id: user.id,
-        name: user.name,
-        tag: user.tag,
-        nickname: user.nickname,
-        username: user.username,
-        xp: user.xp,
-        plus: user.plus,
-        createdAt: user.createdAt
-    };
-    return {
-        id: user.id,
-        name: user.name,
-        tag: user.tag,
-        username: user.username,
-        data: JSON.stringify(data),
-        cachedAt: new Date()
-    };
+const getUserFromNexus = async (id) => {
+  const user = await nexus(`users/${encodeURIComponent(id)}`);
+  const data = {
+    id: user.id,
+    name: user.name,
+    tag: user.tag,
+    nickname: user.nickname,
+    username: user.username,
+    xp: user.xp,
+    plus: user.plus,
+    createdAt: user.createdAt,
+  };
+  return {
+    id: user.id,
+    name: user.name,
+    tag: user.tag,
+    username: user.username,
+    data: JSON.stringify(data),
+    cachedAt: new Date(),
+  };
 };
 
 // Get User
 const getUser = async (query, id, res) => {
-    let user = await User.findOne({where: query});
+  let user = await User.findOne({ where: query });
 
-    try {
-        if (!user) user = await User.create(await getUserFromNexus(await id()));
-        else if (user.cachedAt < new Date().getTime() - 10000) await user.update(await getUserFromNexus(await id()))
-    } catch (err) {}
+  try {
+    if (!user) user = await User.create(await getUserFromNexus(await id()));
+    else if (user.cachedAt < new Date().getTime() - 10000)
+      await user.update(await getUserFromNexus(await id()));
+  } catch (err) {}
 
-    if (user) res.json({
-        ...JSON.parse(user.data),
-        cachedAt: user.cachedAt
+  if (user)
+    res.json({
+      ...JSON.parse(user.data),
+      cachedAt: user.cachedAt,
     });
-    else res.status(404).json({err: "missingResource"});
+  else res.status(404).json({ err: "missingResource" });
 };
 
 // Get User from Name#tag
-app.get("/nametag/:name/:tag", (req, res) =>    
-    getUser(
-        {
-            name: req.params.name,
-            tag: req.params.tag
-        },
-        async () =>
-            (await nexus(`nametag?name=${encodeURIComponent(req.query.name)}&tag=${encodeURIComponent(req.query.tag)}`)).id,
-        res
-    )
+app.get("/nametag/:name/:tag", (req, res) =>
+  getUser(
+    {
+      name: req.params.name,
+      tag: req.params.tag,
+    },
+    async () =>
+      (
+        await nexus(
+          `nametag?name=${encodeURIComponent(
+            req.query.name
+          )}&tag=${encodeURIComponent(req.query.tag)}`
+        )
+      ).id,
+    res
+  )
 );
 
 // Get User from Username
 app.get("/username/:username", (req, res) =>
-    getUser(
-        {username: req.params.username},
-        async () =>
-            (await nexus(`username/${encodeURIComponent(req.params.username)}`)).id,
-        res
-    )
+  getUser(
+    { username: req.params.username },
+    async () =>
+      (await nexus(`username/${encodeURIComponent(req.params.username)}`)).id,
+    res
+  )
 );
 
 // Get User from ID
 app.get("/users/:id", (req, res) =>
-    getUser(
-        {id: req.params.id},
-        () => req.params.id,
-        res
-    )
+  getUser({ id: req.params.id }, () => req.params.id, res)
 );
 
 // 404
-app.use((_req, res) => res.status(404).json({err: "notFound"}));
+app.use((_req, res) => res.status(404).json({ err: "notFound" }));
