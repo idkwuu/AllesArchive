9b7f700851f8d49ddce11ed806f64ba21f0acc26
Archie Baer <archie@abaer.dev>
2020-11-21 18:39:23 +0000

    Create friendship endpoint no longer uses body

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const friendLimit = 25;
 // Express
 const express = require("express");
 const app = express();
-app.use(require("body-parser").json());
-app.use((_err, _req, res, _next) =>
-  res.status(500).json({ err: "internalError" })
-);
 app.listen(8080, () => db.sync().then(() => console.log("Server is ready")));
 
 // Auth
@@ -71,10 +67,8 @@ app.get("/:id", async (req, res) => {
 });
 
 // Create/Accept Friend Request
-app.post("/:id", async (req, res) => {
-  if (typeof req.body.user !== "string")
-    return res.status(400).json({ err: "badRequest" });
-  if (req.params.id === req.body.user)
+app.post("/:id/:friend", async (req, res) => {
+  if (req.params.id === req.params.friend)
     return res.status(400).json({ err: "user.friend.self" });
 
   // Friend Count = Active Friends + Outgoing Requests
@@ -98,7 +92,7 @@ app.post("/:id", async (req, res) => {
   // Check for incoming request
   const incomingRequest = await db.Friendship.findOne({
     where: {
-      user1: req.body.user,
+      user1: req.params.friend,
       user2: req.params.id,
       acceptedAt: null,
     },
@@ -112,8 +106,8 @@ app.post("/:id", async (req, res) => {
   const alreadyExists = await db.Friendship.findOne({
     where: {
       [Op.or]: [
-        { user1: req.params.id, user2: req.body.user },
-        { user1: req.body.user, user2: req.params.id },
+        { user1: req.params.id, user2: req.params.friend },
+        { user1: req.params.friend, user2: req.params.id },
       ],
     },
   });
@@ -123,7 +117,7 @@ app.post("/:id", async (req, res) => {
   const request = await db.Friendship.create({
     id: uuid(),
     user1: req.params.id,
-    user2: req.body.user,
+    user2: req.params.friend,
     requestedAt: new Date(),
   });
 
@@ -143,7 +137,7 @@ app.delete("/:id/:friend", async (req, res) => {
         },
         {
           user1: req.params.friend,
-          user2: req.body.user,
+          user2: req.params.friend,
         },
       ],
     },
