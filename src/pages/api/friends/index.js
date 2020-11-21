815164fc2d117fd01ea910b9e31f3f6f7687edfd
Archie Baer <archie@abaer.dev>
2020-11-21 17:37:04 +0000

    No longer orders friends (API already does it)

--- src/pages/api/friends/index.js
+++ src/pages/api/friends/index.js
@@ -17,21 +17,15 @@ export default async (req, res) => {
 								Authorization: FRIENDS_SECRET,
 							},
 						})
-					).data.friends
-						.sort(
-							(a, b) =>
-								new Date(a.acceptedAt).getTime() -
-								new Date(b.acceptedAt).getTime()
-						)
-						.map(async ({ user }) => {
-							try {
-								return (
-									await axios.get(
-										`${HORIZON_API}/users/${encodeURIComponent(user)}`
-									)
-								).data;
-							} catch (err) {}
-						})
+					).data.friends.map(async ({ user }) => {
+						try {
+							return (
+								await axios.get(
+									`${HORIZON_API}/users/${encodeURIComponent(user)}`
+								)
+							).data;
+						} catch (err) {}
+					})
 				)
 			).filter((u) => !!u),
 		});
