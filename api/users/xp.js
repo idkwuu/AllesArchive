224ffc85ada1fcee39462971679a2170bc5caaec
Archie Baer <archie@abaer.dev>
2020-11-28 21:56:20 +0000

    Automatically adds xp when users level up

--- api/users/xp.js
+++ api/users/xp.js
@@ -1,5 +1,6 @@
 const db = require("../../db");
 const { literal } = require("sequelize");
+const getLevel = require("../../util/level");
 
 module.exports = async (req, res) => {
   if (typeof req.body.xp !== "number")
@@ -13,9 +14,14 @@ module.exports = async (req, res) => {
   });
   if (!user) return res.status(404).json({ err: "missingResource" });
 
+  // Check level difference
+  const levels =
+    getLevel(user.xp + req.body.xp).level - getLevel(user.xp).level;
+
   // Update
   await user.update({
     xp: literal(`xp + ${req.body.xp}`),
+    coins: levels > 0 ? literal(`coins + ${levels * 10}`) : undefined,
   });
 
   // Response
