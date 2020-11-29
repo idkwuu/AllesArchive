506a33ad7917c8719334dcfcfbbaa398cb30aecd
Archie Baer <archie@abaer.dev>
2020-11-29 01:34:00 +0000

    PurchaseMembership component

--- src/pages/plus.js
+++ src/pages/plus.js
@@ -9,26 +9,6 @@ import config from "../config";
 
 const page = () => {
 	const user = useUser();
-	const [loading, setLoading] = useState(false);
-	const [done, setDone] = useState(false);
-
-	const subscribe = (option) => {
-		setLoading(true);
-		axios
-			.post(
-				"/api/plus",
-				{
-					option,
-				},
-				{
-					headers: {
-						Authorization: user.sessionToken,
-					},
-				}
-			)
-			.then(() => setDone(true))
-			.catch(() => {});
-	};
 
 	return (
 		<Page title="Plus" breadcrumbs={<Breadcrumb.Item>Plus</Breadcrumb.Item>}>
@@ -67,61 +47,86 @@ const page = () => {
 				</Box.Content>
 			</Box>
 
-			<Box>
-				<Box.Header>
-					{user.plus.active ? "Extend" : "Start"} Membership
-				</Box.Header>
-				{done ? (
-					<Box.Content className="space-y-3">
-						{user.plus ? (
-							<p>
-								Thanks, {user.nickname}! You've just extended your subscription,
-								so now you get to enjoy your Alles+ benefits for even longer :)
-							</p>
-						) : (
-							<p>
-								Welcome to the club, {user.nickname}! You now get to enjoy the
-								great Alles+ benefits that we've had waiting for you :)
-							</p>
-						)}
-					</Box.Content>
-				) : (
-					<Box.Content className="space-y-6">
-						<p>
-							You can start or extend your membership. It won't renew
-							automatically, you can just come back here to add some time to it.
-							You get Alles+ using Coins, which you can learn more about on{" "}
-							<Link href="/coins">
-								<a className="text-primary">the Coins page</a>
-							</Link>
-							. You have{" "}
-							<strong className="border-b border-primary">
-								{user.coins} coin{user.coins === 1 ? "" : "s"}
-							</strong>{" "}
-							right now.
-						</p>
-						<div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-							{Object.keys(config.plusOptions).map((id) => {
-								const option = config.plusOptions[id];
-								return (
-									<Button
-										key={id}
-										size="md"
-										className="w-full"
-										onClick={() => subscribe(id)}
-										loading={loading}
-										disabled={user.coins < option.coins}
-									>
-										{option.name} ({option.coins} coin
-										{option.coins === 1 ? "" : "s"})
-									</Button>
-								);
-							})}
-						</div>
-					</Box.Content>
-				)}
-			</Box>
+			<PurchaseMembership user={user} />
 		</Page>
+	);
+};
+
+const PurchaseMembership = ({ user }) => {
+	const [loading, setLoading] = useState(false);
+	const [done, setDone] = useState(false);
+
+	const subscribe = (option) => {
+		setLoading(true);
+		axios
+			.post(
+				"/api/plus",
+				{ option },
+				{
+					headers: {
+						Authorization: user.sessionToken,
+					},
+				}
+			)
+			.then(() => setDone(true))
+			.catch(() => {});
+	};
+
+	return (
+		<Box>
+			<Box.Header>
+				{user.plus.active ? "Extend" : "Start"} Membership
+			</Box.Header>
+			{done ? (
+				<Box.Content className="space-y-3">
+					{user.plus ? (
+						<p>
+							Thanks, {user.nickname}! You've just extended your subscription,
+							so now you get to enjoy your Alles+ benefits for even longer :)
+						</p>
+					) : (
+						<p>
+							Welcome to the club, {user.nickname}! You now get to enjoy the
+							great Alles+ benefits that we've had waiting for you :)
+						</p>
+					)}
+				</Box.Content>
+			) : (
+				<Box.Content className="space-y-6">
+					<p>
+						You can start or extend your membership. It won't renew
+						automatically, you can just come back here to add some time to it.
+						You get Alles+ using Coins, which you can learn more about on{" "}
+						<Link href="/coins">
+							<a className="text-primary">the Coins page</a>
+						</Link>
+						. You have{" "}
+						<strong className="border-b border-primary">
+							{user.coins} coin{user.coins === 1 ? "" : "s"}
+						</strong>{" "}
+						right now.
+					</p>
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+						{Object.keys(config.plusOptions).map((id) => {
+							const option = config.plusOptions[id];
+							return (
+								<Button
+									key={id}
+									size="md"
+									className="w-full"
+									onClick={() => subscribe(id)}
+									loading={loading}
+									disabled={user.coins < option.coins}
+								>
+									{option.name} ({option.coins} coin
+									{option.coins === 1 ? "" : "s"})
+								</Button>
+							);
+						})}
+					</div>
+				</Box.Content>
+			)}
+		</Box>
 	);
 };
 
