d51202440c3139cab506f0c2beeb32eaabac6bff
Archie Baer <archie@abaer.dev>
2020-11-29 00:57:57 +0000

    Coins deducted and membership extended

--- src/pages/api/plus.js
+++ src/pages/api/plus.js
@@ -12,9 +12,47 @@ export default async (req, res) => {
 	if (typeof req.body.option !== "string")
 		return res.status(400).json({ err: "badRequest" });
 
+	// Get Option
 	const option = config.plusOptions[req.body.option];
 	if (!option) return res.status(400).json({ err: "badRequest" });
+	if (option.coins > user.coins)
+		return res.status(400).json({ err: "user.coins.notEnough" });
 
-	console.log(option);
-	res.json({});
+	try {
+		// Deduct Coins
+		await axios.post(
+			`${NEXUS_URI}/users/${user.id}/coins`,
+			{
+				coins: 0 - option.coins,
+			},
+			{
+				auth: {
+					username: NEXUS_ID,
+					password: NEXUS_SECRET,
+				},
+			}
+		);
+
+		// Set Membership End
+		await axios.post(
+			`${NEXUS_URI}/users/${user.id}`,
+			{
+				plusEnd: new Date(
+					(user.plus.active ? new Date(user.plus.end) : new Date()).getTime() +
+						option.days * 24 * 60 * 60 * 1000
+				),
+			},
+			{
+				auth: {
+					username: NEXUS_ID,
+					password: NEXUS_SECRET,
+				},
+			}
+		);
+
+		// Response
+		res.json({});
+	} catch (err) {
+		res.status(500).json({ err: "internalError" });
+	}
 };
