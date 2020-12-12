d6599c553cc67e2fdeb9744b36b781feb701eaae
Archie Baer <archie@abaer.dev>
2020-12-12 20:53:42 +0000

    Download button

--- src/pulsar.js
+++ src/pulsar.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { Spectrum } from "./spectrum";
 
+const latest = "0.0.2";
+const ua = navigator.userAgent;
+const platform = ua.includes("Windows NT")
+  ? ["win32", "Windows"]
+  : ua.includes("Linux") && !ua.includes("Mobile")
+  ? ["linux", "Linux"]
+  : ua.includes("Macintosh")
+  ? ["darwin", "macOS"]
+  : null;
+
 export const Pulsar = ({
   query,
   placeholder,
@@ -13,7 +23,24 @@ export const Pulsar = ({
       <input placeholder={placeholder} value={query} disabled />
     </form>
 
-    {!query && <Spectrum />}
+    {!query && (
+      <>
+        <Spectrum />
+        <div className="banner">
+          <p>
+            Get Pulsar{platform && ` for ${platform[1]}`} to start searching
+          </p>
+          <a
+            className="button"
+            href={`https://files.alles.cc/Pulsar/${latest}${
+              platform ? `/pulsar-${platform[0]}-x64.zip` : ``
+            }`}
+          >
+            Download
+          </a>
+        </div>
+      </>
+    )}
 
     {answer && <div className="answer">{answer}</div>}
 
